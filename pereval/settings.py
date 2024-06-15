ab75648ef30c3f81887feb782af030ce6ceb2194
Alexander <alexandradaniloff@gmail.ru>
2024-06-15 21:38:45 +0300

    swagger не отображается

--- pereval/settings.py
+++ pereval/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'pereval.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
--- project/urls.py
+++ project/urls.py
@@ -2,12 +2,7 @@ from django.urls import path, include, re_path
 from django.contrib import admin
 from rest_framework import routers, permissions
 from project import views
-from rest_framework.routers import DefaultRouter
-from django.views.generic import TemplateView
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-
+from pereval.yasg import urlpatterns as doc_urls
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UsersViewset, basename='users')
@@ -17,5 +12,7 @@ router.register(r'images', views.ImagesViewset, basename='images')
 router.register(r'perevals', views.PerevalViewset, basename='perevals')
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include(router.urls)),
 ]
+urlpatterns += doc_urls
